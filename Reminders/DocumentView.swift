2b8f3b015241ef8d384f0f51e3188137f8d0325a
Mckiba <wmckiba@gmail.com>
2020-09-06 20:28:40 -0500

    Add Document View Add Task List and fuctionality to the All section of the Document View

---
+++ Reminders/DocumentView.swift
@@ -0,0 +1,21 @@
+//
+//  DocumentView.swift
+//  Reminders
+//
+//  Created by McKiba Williams on 9/5/20.
+//  Copyright © 2020 McKiba Williams. All rights reserved.
+//
+
+import SwiftUI
+
+struct DocumentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DocumentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DocumentView()
+    }
+}
